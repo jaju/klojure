7915c481c5446867d0feedc8a256257bece3fe8d
Ravindra Jaju <jaju@msync.org>
2023-02-27 16:18:56 +0530

    A few trial runs - of invoking core clojure functions

--- lib/src/main/kotlin/org/msync/klojure/Klojure.kt
+++ lib/src/main/kotlin/org/msync/klojure/Klojure.kt
@@ -1,7 +1,9 @@
 package org.msync.klojure
 
 import clojure.java.api.Clojure
+import clojure.lang.ISeq
 import clojure.lang.Keyword
+import clojure.lang.LazySeq
 import clojure.lang.Symbol
 
 object Klojure {
@@ -16,9 +18,38 @@ object Klojure {
 
     // Fn invocation
     fun fn(s: String) = Clojure.`var`(s)
+    fun fn(ns: String, s: String) = Clojure.`var`(ns, s)
+    private fun cfn(s: String) = fn("clojure.core", s)
+
+    val _apply = cfn("apply")
+    // Core imports
+    val map = cfn("map")
+    val list = cfn("list")
+    val reduce = cfn("reduce")
+    val plus = cfn("+")
+    val inc = cfn("inc")
+
+    fun <T> apply(vararg args: Any): T? {
+        assert(args.size > 1)
+        return when (args.size) {
+            2 -> _apply(args[0], args[1])
+            3 -> _apply(args[0], args[1], args[2])
+            else -> null
+        } as T
+    }
 }
 
-fun main(args: Array<String>) {
-    val f = Klojure.fn("clojure.core/+")
+fun <T> Any.seq(): T? = if (this is LazySeq) this.seq() as T else throw Exception("${this.javaClass} is not a LazySeq")
+
+fun <T> ISeq.list() = Klojure.apply<T>(Klojure.list, this)
+fun main() {
+    val f = Klojure.fn("clojure.core","+")
     println(f(1, 2))
+
+    println(Klojure.reduce(Klojure.plus, listOf(1, 2, 3, 4, 5)))
+    println(Klojure.map(Klojure.inc, listOf(1, 2, 3, 4, 5)).seq<List<Long>>())
+    println(Klojure.map(Klojure.inc, listOf(1, 2, 3, 4, 5)).seq<List<Long>>())
+
+    println(Klojure.apply(Klojure.plus, listOf(1,2,3)))
+
 }
--- lib/src/test/kotlin/org/msync/klojure/KlojureTest.kt
+++ lib/src/test/kotlin/org/msync/klojure/KlojureTest.kt
@@ -3,13 +3,27 @@
  */
 package org.msync.klojure
 
+import org.msync.klojure.Klojure.fn
+import org.msync.klojure.Klojure.inc
+import org.msync.klojure.Klojure.map
+import org.msync.klojure.Klojure.plus
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertTrue
 
 class KlojureTest {
     @Test fun `invokes the clojure + function`() {
-        val f = Klojure.fn("clojure.core/+")
+        val f = fn("clojure.core/+")
         assertEquals(f(1, 2), 3L,  "Did not add up...")
+    }
+
+    @Test fun `use an imported function`() {
+        assertEquals(plus(1, 2), 3L, "Could not invoke plus...")
+    }
+
+    @Test fun `invoke a function that returns a lazy-seq, and seq() on it`() {
+        val myNumbers = listOf(1, 2, 3, 4, 5)
+        val results = map(inc, myNumbers)
+        println(results.seq<List<Long>>()?.javaClass)
+        assertEquals(listOf(2L,3L,4L,5L,6L), results.seq<List<Long>>())
     }
 }
