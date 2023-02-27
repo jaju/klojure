7a4da88a72c5f1a951aa09534d49f93bc90318df
Ravindra Jaju <jaju@sahaj.ai>
2023-02-27 20:08:45 +0530

    A good run - multiple examples (in tests) of interacting with Clojure. More to come.

--- lib/src/main/kotlin/org/msync/klojure/Klojure.kt
+++ lib/src/main/kotlin/org/msync/klojure/Klojure.kt
@@ -1,55 +1,62 @@
 package org.msync.klojure
 
 import clojure.java.api.Clojure
-import clojure.lang.ISeq
+import clojure.lang.IFn
 import clojure.lang.Keyword
 import clojure.lang.LazySeq
 import clojure.lang.Symbol
 
-object Klojure {
+// Symbols
+fun symbol(s: String): Symbol = Symbol.intern(s)
+fun symbol(k: Keyword): Symbol = k.sym
 
-    // Symbols
-    fun symbol(s: String): Symbol = Symbol.intern(s)
-    fun symbol(k: Keyword): Symbol = k.sym
+// Keywords
+fun keyword(s: String): Keyword = Keyword.intern(s)
+fun keyword(k: Keyword): Keyword = k
 
-    // Keywords
-    fun keyword(s: String): Keyword = Keyword.intern(s)
-    fun keyword(k: Keyword): Keyword = k
+// Fn invocation
+fun fn(s: String): IFn = Clojure.`var`(s)
+fun fn(ns: String, s: String): IFn = Clojure.`var`(ns, s)
 
-    // Fn invocation
-    fun fn(s: String) = Clojure.`var`(s)
-    fun fn(ns: String, s: String) = Clojure.`var`(ns, s)
-    private fun cfn(s: String) = fn("clojure.core", s)
+private fun cfn(s: String) = fn("clojure.core", s)
+private val _apply = cfn("apply")
 
-    val _apply = cfn("apply")
-    // Core imports
-    val map = cfn("map")
-    val list = cfn("list")
-    val reduce = cfn("reduce")
-    val plus = cfn("+")
-    val inc = cfn("inc")
+// Core imports
+val _readString = cfn("read-string")
+val _eval = cfn("eval")
+val list = cfn("list")
+val map = cfn("map")
+val reduce = cfn("reduce")
+val get = cfn("get")
+val selectKeys = cfn("select-keys")
 
-    fun <T> apply(vararg args: Any): T? {
-        assert(args.size > 1)
-        return when (args.size) {
-            2 -> _apply(args[0], args[1])
-            3 -> _apply(args[0], args[1], args[2])
-            else -> null
-        } as T
-    }
+fun <T> eval(s: String): T = _eval(_readString(s)) as T
+
+fun <T> Any.seq(): T = (this as LazySeq).seq() as T
+
+fun <T> apply(vararg a: Any): T {
+    assert(a.size > 1)
+    return when (a.size) {
+        2 -> _apply(a[0], a[1])
+        3 -> _apply(a[0], a[1], a[2])
+        4 -> _apply(a[0], a[1], a[2], a[3])
+        5 -> _apply(a[0], a[1], a[2], a[3], a[4])
+        6 -> _apply(a[0], a[1], a[2], a[3], a[4], a[5])
+        7 -> _apply(a[0], a[1], a[2], a[3], a[4], a[5], a[6])
+        8 -> _apply(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7])
+        9 -> _apply(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8])
+        10 -> _apply(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9])
+        11 -> _apply(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10])
+        12 -> _apply(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11])
+        13 -> _apply(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12])
+        14 -> _apply(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13])
+        15 -> _apply(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], a[14])
+        16 -> _apply(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], a[14], a[15])
+        17 -> _apply(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], a[14], a[15], a[16])
+        18 -> _apply(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], a[14], a[15], a[16], a[17])
+        19 -> _apply(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], a[14], a[15], a[16], a[17], a[18])
+        20 -> _apply(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], a[14], a[15], a[16], a[17], a[18], a[19])
+        else -> _apply(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], a[14], a[15], a[16], a[17], a[18], a[19], a.copyOfRange(20, a.size))
+    } as T
 }
 
-fun <T> Any.seq(): T? = if (this is LazySeq) this.seq() as T else throw Exception("${this.javaClass} is not a LazySeq")
-
-fun <T> ISeq.list() = Klojure.apply<T>(Klojure.list, this)
-fun main() {
-    val f = Klojure.fn("clojure.core","+")
-    println(f(1, 2))
-
-    println(Klojure.reduce(Klojure.plus, listOf(1, 2, 3, 4, 5)))
-    println(Klojure.map(Klojure.inc, listOf(1, 2, 3, 4, 5)).seq<List<Long>>())
-    println(Klojure.map(Klojure.inc, listOf(1, 2, 3, 4, 5)).seq<List<Long>>())
-
-    println(Klojure.apply(Klojure.plus, listOf(1,2,3)))
-
-}
